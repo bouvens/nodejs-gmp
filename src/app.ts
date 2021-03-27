c2d8aba0757a3df58cfbd57fb000c36ed5eb2bea
Aleksandr Demin <Aleksandr_Demin1@epam.com>
2021-03-27 18:04:27 +0600

    feat: Create echo server

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,29 @@ import express, { Express } from 'express';
 const PORT = process.env.PORT || 3000;
 
 const app: Express = express();
-app.listen(PORT);
-console.log('Server is running on', PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.setHeader('X-Powered-By', 'gmp');
+  next();
+});
+
+// it's easier to use just req.params.id
+app.param('id', (req, res, next, id) => {
+  req.id = id;
+  next();
+});
 
 app.get('/', (req, res) => {
-  res.json(req.headers);
+  const { headers, body, query } = req;
+  res.json({ headers, body, query });
+});
+
+app.get('/user/:id', (req, res) => {
+  const userId: number = parseInt(req.id, 10);
+  res.json({ userId });
 });
