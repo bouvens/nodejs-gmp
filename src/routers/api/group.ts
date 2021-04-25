c16e8d6886f0b19feea18debc6968ef0ca7cc692
Aleksandr Demin <Aleksandr_Demin1@epam.com>
2021-04-25 14:53:34 +0600

    feat: Add an end-point for addUsersToGroup()

--- src/routers/api/group.ts
+++ src/routers/api/group.ts
@@ -1,8 +1,9 @@
 import GroupService from '../../services/group';
 import { groupModel } from '../../models/group';
-import { group } from './validation';
+import { addUsersToGroup, group } from './validation';
 import { makeCrudRouter } from './crud';
 import { OpenGroupProps } from '../../types';
+import { AppError, ErrorStatus } from '../../services/error';
 
 const groupService = new GroupService(groupModel);
 const router = makeCrudRouter<OpenGroupProps, GroupService, group.ValidatedRequest>(
@@ -19,5 +20,25 @@ router.get('/', async (req, res, next) => {
     next(e);
   }
 });
+
+// Add users to a group
+router.put(
+  '/:id/add-users',
+  addUsersToGroup.validator,
+  async (req: addUsersToGroup.ValidatedRequest, res, next) => {
+    try {
+      const { id } = req.params;
+      const { users } = req.body;
+      const status = await groupService.addUsersToGroup(id, users);
+      if (status) {
+        next(new AppError(status, ErrorStatus.internal));
+        return;
+      }
+      res.json({ status: 'success' });
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
 export default router;
--- src/services/group.ts
+++ src/services/group.ts
@@ -1,10 +1,16 @@
 import { IGroup, OpenGroupProps } from '../types';
 import { GroupModel } from '../models/group';
 import CrudService from './crud';
+import { IBasicItem } from '../types/common';
+import { usersGroupModel } from '../models/users_groups';
 
 export default class GroupService extends CrudService<OpenGroupProps, GroupModel> {
   async getAll(): Promise<IGroup[]> {
     const groups = await this.model.findAll();
     return groups || [];
+  }
+
+  async addUsersToGroup(id: IBasicItem['id'], userIDs: IBasicItem['id'][]): Promise<void | string> {
+    return await usersGroupModel.addUsersToGroup(id, userIDs);
   }
 }
