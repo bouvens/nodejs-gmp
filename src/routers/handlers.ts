fd042580defefc545d5d5c43f135155e10dae3f4
Aleksandr Demin <Aleksandr_Demin1@epam.com>
2021-05-19 22:50:25 +0600

    fix: Handle async errors properly

--- src/routers/handlers.ts
+++ src/routers/handlers.ts
@@ -3,4 +3,4 @@ import asyncHandler from 'express-async-handler';
 import { withLogger } from '../logger';
 
 export const withHandlers = (serviceMethod: string) => (func: RequestHandler): RequestHandler =>
-  asyncHandler(withLogger(serviceMethod)(func));
+  withLogger(serviceMethod)(asyncHandler(func));
