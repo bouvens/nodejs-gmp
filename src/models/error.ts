d2b2dbf873df75d1c1de4a4666fe0fac76e1bb37
Aleksandr Demin <Aleksandr_Demin1@epam.com>
2021-05-24 19:39:41 +0600

    feat: Generate and check JWT in service, log warns

--- src/models/error.ts
+++ src/models/error.ts
@@ -34,21 +34,37 @@ const makeInternalError = (methodName: string, args: IDetails['args']) => (
   throw new InternalError(e.message, { methodName, args });
 };
 
-export function wrapErrorsAndLog<R>(
-  target: unknown,
-  propertyKey: string,
-  descriptor: PropertyDescriptor,
-): PropertyDescriptor {
-  const methodName = `${target.constructor.name}.${propertyKey}`;
-  const originalMethod = descriptor.value;
-  return {
-    ...descriptor,
-    value: function (...args: unknown[]): R {
-      const start = process.hrtime.bigint();
-      return originalMethod
-        .call(this, ...args)
-        .catch(makeInternalError(methodName, args))
-        .finally(logTime(start, methodName, args));
-    },
+function makeWrapperAndLogger<R>(withArgs = false) {
+  return (
+    target: unknown,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ): PropertyDescriptor => {
+    const methodName = `${target.constructor.name}.${propertyKey}`;
+    const originalMethod = descriptor.value;
+    return {
+      ...descriptor,
+      value: function (...args: unknown[]): R {
+        const safeArgs = withArgs ? args : [{ safe: true }];
+        const start = process.hrtime.bigint();
+        const internalError = makeInternalError(methodName, safeArgs);
+        const end = logTime(start, methodName, safeArgs);
+        let result;
+        try {
+          result = originalMethod.call(this, ...args);
+        } catch (e) {
+          internalError(e);
+        }
+        if (result.catch) {
+          return result.catch(internalError).finally(end);
+        }
+        end();
+        return result;
+      },
+    };
   };
 }
+
+export const wrapErrorsAndLogSafely = makeWrapperAndLogger();
+
+export const wrapErrorsAndLog = makeWrapperAndLogger(true);
