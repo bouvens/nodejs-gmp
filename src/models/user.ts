bdd6cbdfbbe4e420e7f903a77dfec1c5714c9d0f
Alex Demin <bouvens@gmail.com>
2021-06-02 01:05:21 +0600

    fix: Return number for a user age

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,24 +1,8 @@
-import { BOOLEAN, NUMBER, STRING, UUIDV4, Op } from 'sequelize';
-import { OpenUserProps, IUser } from '../types';
-import { sequelize } from '../data-access/postgresql';
+import { Op } from 'sequelize';
+import { IUser, OpenUserProps } from '../types';
 import { getPlainAndFiltered } from './common';
 import { CrudModel } from './crud';
 import { IBasicItem } from '../types/common';
-
-const User = sequelize.define(
-  'user',
-  {
-    id: { type: UUIDV4, primaryKey: true },
-    login: { type: STRING, allowNull: false },
-    password: { type: STRING, allowNull: false },
-    age: { type: NUMBER, allowNull: false },
-    isDeleted: { type: BOOLEAN, allowNull: false },
-  },
-  {
-    timestamps: false,
-    tableName: 'users',
-  },
-);
 
 export class UserModel extends CrudModel<OpenUserProps> {
   async findByLogin(login: string): Promise<OpenUserProps & IBasicItem> {
@@ -37,5 +21,3 @@ export class UserModel extends CrudModel<OpenUserProps> {
       .then((users) => users.map(getPlainAndFiltered));
   }
 }
-
-export const userModel = new UserModel(User, true);
