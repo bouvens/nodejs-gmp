570ab0bffa80d6a3691b3174592e8ccd837314be
Aleksandr Demin <Aleksandr_Demin1@epam.com>
2021-06-16 23:44:07 +0600

    refactor: Extract loaders

--- src/data-access/postgresql.ts
+++ src/data-access/postgresql.ts
@@ -1,15 +1,4 @@
 import { Sequelize } from 'sequelize';
 import config from '../config';
-import logger from '../services/logger';
 
 export const sequelize = new Sequelize(config.dbUri, { dialect: 'postgres' });
-
-sequelize
-  .authenticate()
-  .then(() => {
-    logger.info('Connection has been established successfully');
-  })
-  .catch((e) => {
-    logger.error(`DB connection error: ${e?.name}`);
-    process.exit(5);
-  });
